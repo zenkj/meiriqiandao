fc5e0c0da3bd2cc9deb74b7fe7871f1f3f231fc0
zenk <juzejian@gmail.com>
2015-10-08 00:39:49 +0800

    create express scaffold

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '每日签到' });
+});
+
+module.exports = router;
